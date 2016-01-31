453d6b2279a7de89345a3b2ae62cb30f4e66af75
shuvradeb-kar <shuvradeb.kar@gmail.com>
2016-01-31 18:32:17 +0600

    s

---
+++ FruitsRetailer/Server/Controller/CommonController.cs
@@ -0,0 +1,26 @@
+﻿using FruitsRetailer.Server.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FruitsRetailer.Server.Controller
+{
+    public class CommonController : ApiController
+    {
+        [HttpGet]
+        public Dictionary<string, string> GetCustomeValues() {
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            list.Add("CompanyName", FruitsHelper.CompanyName);
+            list.Add("CompanySlogan", FruitsHelper.CompanySlogan);
+            list.Add("CompanyProprietor", FruitsHelper.CompanyProprietor);
+            list.Add("CompanyAddress", FruitsHelper.CompanyAddress);
+            list.Add("GodName", FruitsHelper.GodName);
+            list.Add("ProprietorContactNumber", FruitsHelper.ProprietorContactNumber);
+            
+            return list;
+        }
+    }
+}
--- FruitsRetailer/Server/Util/FruitsHelper.cs
+++ FruitsRetailer/Server/Util/FruitsHelper.cs
@@ -6,5 +6,12 @@ namespace FruitsRetailer.Server.Util
     public class FruitsHelper
     {
         public static string DatabaseConnectionString = ConfigurationManager.AppSettings["DatabaseConnectionString"];
+
+        public static string CompanyName = ConfigurationManager.AppSettings["CompanyName"];
+        public static string CompanySlogan = ConfigurationManager.AppSettings["CompanySlogan"];
+        public static string CompanyProprietor = ConfigurationManager.AppSettings["CompanyProprietor"];
+        public static string CompanyAddress = ConfigurationManager.AppSettings["CompanyAddress"];
+        public static string GodName = ConfigurationManager.AppSettings["GodName"];
+        public static string ProprietorContactNumber = ConfigurationManager.AppSettings["ProprietorContactNumber"];
     }
 }
